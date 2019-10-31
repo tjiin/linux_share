514d65a071d58f6f88e1bf243fccbd8636344aa5
tj@box <chalkcode@protonmail.com>
2019-10-31 03:17:49 -0700

    lshw

--- misc_unix_cmds1.sh
+++ misc_unix_cmds1.sh
@@ -65,3 +65,8 @@ $ route
 	- $ route | tail -n 1 | awk '{print $8}'
 
 $ dev=`route | tail -n 1 | awk '{print $8}'` && ifconfig $dev | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1
+
+
+$ sudo lshw 
+--> niec wat of looking at hardware, similar to meminfo
+
